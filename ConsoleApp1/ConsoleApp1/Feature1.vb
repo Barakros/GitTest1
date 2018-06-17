c3bd8573509bcc28ac8a5ba4f6017c254e3be8ee
Barak Rosenfeld <barakros@gmail.com>
2018-06-17 15:14:29 +0300

    Added Add function

--- ConsoleApp1/ConsoleApp1/Feature1.vb
+++ ConsoleApp1/ConsoleApp1/Feature1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Feature1
+    Public Function Add() As Integer
+        Dim x1 As Integer = 1
+        Dim x2 As Integer = 2
+        Dim sum As Integer = x1 + x2
+        Return sum
+    End Function
 
 End Class
